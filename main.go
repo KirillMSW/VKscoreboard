7d460da2a8bce7eae7c5aa6e8a64a10f530cb9b2
KirillMSW <kzybrow@yandex.ru>
2023-05-10 22:29:49 +0300

    final version

--- main.go
+++ main.go
@@ -9,8 +9,11 @@ import (
 	"github.com/spf13/viper"
 	"golang.org/x/crypto/bcrypt"
 	"log"
+	"math/rand"
 	"net/http"
 	"sort"
+	"strings"
+	"time"
 )
 
 type userInfo struct {
@@ -23,7 +26,7 @@ type userInfo struct {
 	Crypto         int
 }
 
-var store = sessions.NewCookieStore([]byte("SECRET"))
+var store *sessions.CookieStore
 var db *sql.DB
 
 func main() {
@@ -31,18 +34,22 @@ func main() {
 	fmt.Println("Begining app")
 	router := gin.Default()
 	router.POST("/api/login", login)
+	router.POST("/api/logout", logout)
 	router.POST("/api/register", register)
 	router.GET("/api/secret", secret)
+	router.GET("/api/isloged", isLoged)
 	router.GET("/api/scoreboard", getScoreboard)
-	router.Run("localhost:8080")
+	router.Run("localhost:8050")
 }
 
 func onStartup() {
+	rand.Seed(time.Now().UnixNano())
 	viper.SetConfigFile("secrets.json")
 	err := viper.ReadInConfig()
 	if err != nil {
-		//errors.New("Failed to read config")
+		fmt.Println("Failed to read config")
 	}
+	store = sessions.NewCookieStore([]byte(fmt.Sprintf("%v", viper.Get("cookie_secret"))))
 	cfg := mysql.Config{
 		User:   fmt.Sprintf("%v", viper.Get("db_user")),
 		Passwd: fmt.Sprintf("%v", viper.Get("db_password")),
@@ -59,38 +66,37 @@ func onStartup() {
 func login(c *gin.Context) {
 	login := c.PostForm("login")
 	password := c.PostForm("password")
-	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-	fmt.Println(string(hash))
-	if err != nil {
-		// TODO: Properly handle error
-		log.Fatal(err)
-	}
 
-	//TODO: check if there such user
 	result := db.QueryRow("SELECT password FROM  users WHERE login=?", login)
 
 	var dbPass []byte
-	err = result.Scan(&dbPass)
+	err := result.Scan(&dbPass)
 	if err != nil {
-		c.String(418, "Invalid username or password")
+		c.Header("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.String(400, "Invalid username or password")
 		return
 	}
 	res := bcrypt.CompareHashAndPassword(dbPass, []byte(password))
 
 	if res == nil {
-		session, _ := store.Get(c.Request, "cookie-name")
-		// TODO: set secure to "true"
+		session, _ := store.Get(c.Request, "session")
 		session.Options = &sessions.Options{
 			Path:     "/",
-			MaxAge:   86400 * 7,
+			MaxAge:   60,
 			HttpOnly: true,
+			Secure:   true,
 			SameSite: http.SameSiteStrictMode,
 		}
 		session.Values["authenticated"] = true
 		session.Save(c.Request, c.Writer)
+		c.Header("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Header("Access-Control-Allow-Credentials", "true")
 		c.String(200, "Logged in")
 	} else {
-		c.String(418, "Invalid username or password")
+		c.Header("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.String(400, "Invalid username or password")
 	}
 }
 
@@ -101,22 +107,55 @@ func register(c *gin.Context) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	_, err = db.Exec("INSERT INTO users (login, password) VALUES (?,?)", login, hash)
-	if err != nil {
-		fmt.Println("error")
+	result, err := db.Query("SELECT * FROM  tasks")
+
+	categoriesAmount := make(map[string]int)
+	for result.Next() {
+		var task string
+		var amount int
+		err = result.Scan(&task, &amount)
+		if err != nil {
+			log.Fatal(err)
+		}
+		categoriesAmount[task] = amount
 	}
+
+	_, err = db.Exec("INSERT INTO users VALUES (?,?,?,?,?)", login, hash, rand.Intn(categoriesAmount["Web"]),
+		rand.Intn(categoriesAmount["Stego"]), rand.Intn(categoriesAmount["Crypto"]))
+	if err != nil {
+		c.Header("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		if strings.Contains(err.Error(), "Duplicate entry") {
+			c.String(400, "User exists")
+		}
+		c.Writer.WriteHeader(400)
+	}
+	session, _ := store.Get(c.Request, "session")
+	session.Options = &sessions.Options{
+		Path:     "/",
+		MaxAge:   60,
+		HttpOnly: true,
+		Secure:   true,
+		SameSite: http.SameSiteStrictMode,
+	}
+	session.Values["authenticated"] = true
+	session.Save(c.Request, c.Writer)
+	c.Header("Access-Control-Allow-Origin", "http://localhost:3000")
+	c.Header("Access-Control-Allow-Credentials", "true")
 
 }
 func secret(c *gin.Context) {
-	session, _ := store.Get(c.Request, "cookie-name")
+	session, _ := store.Get(c.Request, "session")
 
 	if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
-		fmt.Printf("not okay")
+		c.Header("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Header("Access-Control-Allow-Credentials", "true")
 		c.String(401, "no flag")
 		return
 	}
 
-	fmt.Printf("okay")
+	c.Header("Access-Control-Allow-Origin", "http://localhost:3000")
+	c.Header("Access-Control-Allow-Credentials", "true")
 	c.String(200, "flag{qwert}")
 }
 
@@ -170,5 +209,37 @@ func getScoreboard(c *gin.Context) {
 		TasksInfo map[string]int
 		UsersInfo []userInfo
 	}{categoriesAmount, users}
+	c.Header("Access-Control-Allow-Origin", "http://localhost:3000")
+	c.Header("Access-Control-Allow-Credentials", "true")
 	c.JSON(200, respStruct)
+}
+
+func isLoged(c *gin.Context) {
+	session, _ := store.Get(c.Request, "session")
+
+	if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
+		c.Header("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Writer.WriteHeader(401)
+		return
+	}
+
+	c.Header("Access-Control-Allow-Origin", "http://localhost:3000")
+	c.Header("Access-Control-Allow-Credentials", "true")
+	c.String(200, "flag{qwert}")
+}
+
+func logout(c *gin.Context) {
+	session, _ := store.Get(c.Request, "session")
+	session.Options = &sessions.Options{
+		Path:     "/",
+		MaxAge:   -1,
+		HttpOnly: true,
+		Secure:   true,
+		SameSite: http.SameSiteStrictMode,
+	}
+	session.Values["authenticated"] = false
+	session.Save(c.Request, c.Writer)
+	c.Header("Access-Control-Allow-Origin", "http://localhost:3000")
+	c.Header("Access-Control-Allow-Credentials", "true")
 }
